3c69e21fdf0f52ea322539348977dcafbad28d93
Steve Jerman <sjerman@gmail.com>
2013-04-18 17:37:03 -0600

    updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,30 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "testappe"
+set :deploy_to, "/home/sjerman/rails"
+set :repository,  "git@github.com:sjerman/test.git"
+set :scm, "git"
+set :deploy_via, :remote_cache
+server "thejermans.com", :app, :web, :db, :primary => true
+set :user, "sjerman"
+set :use_sudo, false
+set :branch, "master"
+set :ssh_options, { :forward_agent => true }
+set :default_environment, { 'PATH' => '/home/sjerman/.gems/bin:/usr/lib/ruby/gems/1.8/bin/:/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games' }
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+require "bundler/capistrano"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+load 'deploy/assets'
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
