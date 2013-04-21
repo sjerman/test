6752ee10a1fd49992bb8689425326499f5653cbc
Steve Jerman <sjerman@gmail.com>
2013-04-21 16:05:51 -0600

    add dgrid

--- app/assets/javascripts/things.js
+++ app/assets/javascripts/things.js
@@ -17,3 +17,20 @@ function(coreFx, dom, domGeom, on){
     slideIt(-200);
   });
 });
+
+require(["dgrid/Grid", "dojo/domReady!"], function(Grid){
+    var data = [
+        { first: "Bob", last: "Barker", age: 89 },
+        { first: "Vanna", last: "White", age: 55 },
+        { first: "Pat", last: "Sajak", age: 65 }
+    ];
+ 
+    var grid = new Grid({
+        columns: {
+            first: "First Name",
+            last: "Last Name",
+            age: "Age"
+        }
+    }, "grid");
+    grid.renderArray(data);
+});
